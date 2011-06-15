a0ac1d4d0df6fec84a181bab70057d623bbc4037
Francisco Souza <francisco@franciscosouza.net>
2011-06-15 18:45:22 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vitrine::Application.config.secret_token = 'd9092a9389087369733f89480a1963378a1ef9e1e219664ab86641822c976f665398c695f2a9bcff6846e6c5e305a0febd6e9bceb517fd835e0585441fe6d779'
